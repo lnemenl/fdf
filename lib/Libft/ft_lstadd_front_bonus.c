b133d0cf6b8009da78b1ccf125326c9814fb2254
Ruslan Khakimullin <ruslankhakimullin@gmail.com>
2024-08-14 09:29:04 +0300

    start

---
+++ lib/Libft/ft_lstadd_front_bonus.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_front_bonus.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rkhakimu <rkhakimu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/27 15:56:57 by rkhakimu          #+#    #+#             */
+/*   Updated: 2024/07/04 19:43:00 by rkhakimu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_front(t_list **lst, t_list *new_node)
+{
+	if (!lst || !new_node)
+		return ;
+	new_node->next = *lst;
+	*lst = new_node;
+}
