9d5ec355180b2c09e5e3c2f49be160c94d590770
Ruslan Khakimullin <ruslankhakimullin@gmail.com>
2024-08-15 14:27:52 +0300

    added fixes

--- src/fdf_menu.c
+++ src/fdf_menu.c
@@ -6,7 +6,7 @@
 /*   By: rkhakimu <rkhakimu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/15 08:17:58 by rkhakimu          #+#    #+#             */
-/*   Updated: 2024/08/15 11:12:17 by rkhakimu         ###   ########.fr       */
+/*   Updated: 2024/08/15 14:24:08 by rkhakimu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,11 @@ void	menu(mlx_t *mlx)
 	
 	x = 20;
 	y = 20;
-	mlx_put_string(mlx, "CONTROLS", x, y);
+	mlx_put_string(mlx, "CONTROLS:", x, y);
 	mlx_put_string(mlx, "COLORS\t\t\t\t\t\t\t\tc", x, y += 35);
 	mlx_put_string(mlx, "ZOOM\t\t\t\t\t\t\t\t\t\tuse mouse scroll or +-", x, y += 20);
-	mlx_put_string(mlx, "MOVE\t\t\t\t\tarrows", x, y += 20);
-	mlx_put_string(mlx, "SCALE HIGHT\t\t\t\t\t\t\t\ts + </>", x, y += 20);
+	mlx_put_string(mlx, "MOVE\t\t\t\t\t\t\t\t\t\tarrows", x, y += 20);
+	mlx_put_string(mlx, "SCALE HIGHT\t\t\ts + </>", x, y += 20);
 	mlx_put_string(mlx, "ROTATE Z\t\t\t\t\t\tz + </>", x, y += 20);
-	mlx_put_string(mlx, "RESET\t\t\t\t\t\t\t\t\tr", x, y += 30);	
+	mlx_put_string(mlx, "RESET\t\t\t\t\t\t\t\t\tr", x, y += 20);	
 }
