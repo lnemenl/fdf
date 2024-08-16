117f61905433888a2abc4464e94680d307f87e5b
Ruslan Khakimullin <rkhakimu@c3r5p5.hive.fi>
2024-08-16 07:30:21 +0300

    minor fix to ft_strchr in Libft

--- lib/Libft/ft_strchr.c
+++ lib/Libft/ft_strchr.c
@@ -3,23 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rkhakimu <rkhakimu@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: rkhakimu <rkhakimu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 18:46:52 by rkhakimu          #+#    #+#             */
-/*   Updated: 2024/05/03 06:08:27 by rkhakimu         ###   ########.fr       */
+/*   Updated: 2024/08/16 07:26:26 by rkhakimu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/*=============================================================================
- * the first occurrence check inside the loop is responsible
- * for returning the pointer to the first occurrence of c
- * in s as soon as it's found. However, if c is not found in s,
- * the loop continues until it reaches the null terminator.
- * After the loop, the second occurrence check is performed to handle the case
- * where c is not present in s, ensuring that the function
- * returns a null pointer to indicate the absence of c in the string.
- *=============================================================================
- */
+#include "libft.h"
 
 char	*ft_strchr(const char *s, int c)
 {
